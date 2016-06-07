a9e1cc445e908f45add4ff5f03b22ad262bc4ee6
hope <hopexiaoqin@icloud.com>
2016-06-07 13:08:38 +0800

    c

--- python/tf-idf/src/hopeComeOn/hope.py
+++ python/tf-idf/src/hopeComeOn/hope.py
@@ -9,6 +9,38 @@ Created on 2016年6月5日
 from util.FileUtil import *
 
 info=u"E:/新建文件夹/新建文件夹"
-oriFile = open(info+"/a.txt")
 dic = readKeyValueFileToDic(info+"/dic.txt")
 print dic
+
+
+oriFile = open(info+"/a.txt")
+topics = open(info+'/result.txt','w')   
+topics2 = open(info+'/result2.txt','w')   
+
+ori_words = oriFile.readlines( )
+for line in ori_words:        
+    line = line.replace('\n','').replace('\r','').replace('\t',' ').replace('\t','\t\t').replace("\xef\xbb\xbf","").strip()
+    if line  == "":
+        continue;   
+    
+    keylistName = ""
+    keylistCode = line.split(' ')
+    for key in keylistCode:
+        keylistName = keylistName + dic[key] + ' '
+    #print line+' '+keylistName
+    r = line+' '+keylistName+'\n'
+    topics.write(r)
+    topics2.write(keylistName+'\n')
+  
+topics.close()
+topics2.close()
+
+print 'ok'
+print u"结果保存路径为 ："+info+"/result1.txt"
+        
+        
+
+
+
+
+
--- python/tf-idf/src/util/FileUtil.py
+++ python/tf-idf/src/util/FileUtil.py
@@ -2,15 +2,20 @@
 #1. the file must be stored as utf-8
 #2. the file is like: 1 a
 #
+#
 def readKeyValueFileToDic(filePath):
     dic={}
     dicFile = open(filePath)
     dic_words = dicFile.readlines( )
     for line in dic_words:        
-        line = line.replace('\n','').replace('\r','').replace('  ',' ').replace('\t\t','\t').replace("\xef\xbb\xbf","").strip()
+        line = line.replace('\n','').replace('\r','').replace('\t',' ').replace('\t','\t\t').replace("\xef\xbb\xbf","").strip()
+        #print line
         if line  == "":
             continue;    
         arr=line.split(' ')
+        #print arr
         dic[arr[0]] = arr[1]
     return dic
+
+
 
